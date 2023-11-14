8b743a96a161ea6c53e4907234c64f8c65c0c462
Wainer <waimaco_88@hotmail.com>
2023-11-13 19:12:24 -0600

    Proyecto

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+  <h5>Buscar: </h5>
+  <input type="text"
+  class="form-control"
+  placeholder="Buscar gift..."
+  (keyup.enter)="searchTag()"
+  #txtTagInput
+>`
+})
+
+export class SearchBoxComponent implements OnInit {
+
+  @ViewChild('txtTagInput')
+  public tagInput!:ElementRef<HTMLInputElement>;
+
+
+  constructor(private gifsService:GifsService) { }
+
+  ngOnInit() { }
+
+  searchTag(){
+    const newTag= this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value='';
+  }
+}
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePagesComponent } from './home-pages/home-pages.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePagesComponent,
+    SearchBoxComponent,
+    CardListComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HomePagesComponent,
+  ]
+})
+export class GifsModule { }
